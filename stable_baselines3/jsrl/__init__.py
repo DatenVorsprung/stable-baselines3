9d2b9d583667035e3c226a7a97aa542d164a0e31
Lukas Linauer <lukas@datenvorsprung.at>
2024-07-08 11:39:09 +0200

    Fix JSSAC imports

--- stable_baselines3/jsrl/__init__.py
+++ stable_baselines3/jsrl/__init__.py
@@ -1,4 +1,4 @@
 from stable_baselines3.sac.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
-from jsrl import JSSAC
+from stable_baselines3.jsrl.jsrl import JSSAC
 
 __all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "JSSAC"]
